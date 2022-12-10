47a459ead0d2331ff1d2e3104d5dd0c1fe22af31
Bilgehan Akcan <bilgehan.akcan@zegteknoloji.com>
2022-12-11 00:38:48 +0300

    backend-contact page connected and registration page validations done

--- uthere/src/Components/ContactPage.js
+++ uthere/src/Components/ContactPage.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import UThere from './UThere';
+import {useState} from 'react';
 import Logout from './Logout';
+import axios from 'axios';
 
 function ContactPage() {
+	const [message, setMessage] = useState("");
+	const [category, setCategory] = useState("");
+
+	function submit() {
+		let item = {category, message};
+		axios.post('http://127.0.0.1:8000/api/', item).then(response => {
+			console.log("success");
+			console.log(response);
+			window.location = "/Dashboard";
+		}).catch(exception => {
+			console.log(exception);
+		});
+	}
+
 	return (
 		<div>
 			<UThere></UThere>
 			<div className='page-background'></div>
 			<Logout></Logout>
 			<div>
-				<form className='contact-form' style={{"background-color": "white"}}>
-					<h1 class="my-1 mr-2">We'd love to hear from you!</h1>
-					<div class="dropdown">
-						<button class="btn btn-light mt-3 dropdown-toggle" style={{"font-size": "20px"}} type="button" id="dropdownMenuButton" data-toggle="dropdown" >Select what we can help you with.</button>
-						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-							<a class="dropdown-item" href="#">Error Reporting</a>
-							<a class="dropdown-item" href="#">Request For A New Feature</a>
-							<a class="dropdown-item" href="#">Other</a>
+				<form className='contact-form' style={{"backgroundColor": "white"}}>
+					<h1 className="my-1 mr-2">We'd love to hear from you!</h1>
+					<div className="dropdown">
+						<button className="btn btn-light mt-3 dropdown-toggle" style={{"fontSize": "20px"}} type="button" id="dropdownMenuButton" data-toggle="dropdown" >Select what we can help you with.</button>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#" onClick={e => setCategory("Error Reporting")}>Error Reporting</a>
+							<a className="dropdown-item" href="#" onClick={e => setCategory("Request For A New Feature")}>Request For A New Feature</a>
+							<a className="dropdown-item" href="#" onClick={e => setCategory("Other")}>Other</a>
 						</div>
 					</div>
-					<label class="my-1 mr-2 mt-3" style={{"font-size": "30px"}}>Message</label>
+					<label class="my-1 mr-2 mt-3" style={{"fontSize": "30px"}}>Message</label>
 					<div class="input-group">
-						<textarea class="form-control mt-1" style={{"height": "150px"}}></textarea>
+						<textarea class="form-control mt-1" style={{"height": "150px"}} onChange={(e) => setMessage(e.target.value)}></textarea>
 					</div>
-					<br></br><center><button type="button" className="btn" style={{ "background-color": "#ffb84d", "color": "white", "font-size": "20px", "width": "150px", "border-radius": "100px"}}>Send</button></center>
+					<br></br><center><button type="button" onClick={submit} className="btn" style={{ "backgroundColor": "#ffb84d", "color": "white", "fontSize": "20px", "width": "150px", "borderRadius": "100px"}}>Send</button></center>
 				</form>
 			</div>
 		</div>
--- uthere/src/settings.js
+++ uthere/src/settings.js
@@ -1,7 +1,7 @@
 import {createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "8e18069b8cd34fc699421190d903f30b";
-const token = "007eJxTYLDTnBicvT566Y/7njknmXS2XrpQv23uTc0G+SQpZ9a+dgsFBotUQwsDM8ski+QUY5O0ZDNLSxMjQ0NLgxRLA+M0Y4Okt439yQ2BjAwNC8+yMjJAIIjPwpCbmJnHwAAAEqwe4g==";
+const token = "007eJxTYDjF++oyp2fRkR/c/uu665oncjzqKZefrPv/pS+XduvnRmYFBotUQwsDM8ski+QUY5O0ZDNLSxMjQ0NLgxRLA+M0Y4OkkkUTkhsCGRkyc4pYGRkgEMRnYchNzMxjYAAAK5oejA==";
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
