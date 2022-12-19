189f9362f3cb770b0aaa2de03cb3a648fd4dfeef
Mustafa Yuşa Babademez <yusa.babademez@ug.bilkent.edu.tr>
2022-12-19 09:47:17 +0300

    Join Meeting with Agora token

--- uthere/src/Components/JoinMeetingPopup.js
+++ uthere/src/Components/JoinMeetingPopup.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function JoinMeetingPopup(props) {
+	const navigate = useNavigate(); 
+
+ 	function joinMeeting() {
+		// TODO: this will be a meeting id, for now it is meeting's token
+    	if (document.querySelector(".form-control").value === "007eJxTYNitcuGg5FJD71SvRqN5X6OEX22Ovi/zqF8gqf7xSouzN54pMBiamidaJpsZJKWZpJgkW5hamBqaWJglJhklm6UYJBsbFU6an9wQyMiwgHcVMyMDBIL4LAy5iZl5DAwAhYkgLw==") {
+			navigate("/Meeting");
+    	}
+		else {
+			alert("Invalid Meeting ID");
+		}
+	}
 
 	function insidePopup() {
 		return (
@@ -10,7 +22,7 @@ function JoinMeetingPopup(props) {
 					<center>
 						<h2>Enter Meeting ID</h2><br></br>
 						<input className="form-control"/><br></br>
-						<button type="button" className="btn btn-primary">Join</button>
+						<button onClick={() => joinMeeting()} className="btn btn-primary">Join</button>
 						<button type="button" onClick={() => props.setTrigger(false)} className="close popup-close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 					</center>
 				</div>
