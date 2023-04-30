8f95345ba6db7874978f3d8ddbb1ea49229f6e8b
Bilgehan Akcan <bilgehan.akcan@zegteknoloji.com>
2023-04-30 03:56:23 +0300

    credentials changed

--- uthere/src/settings.js
+++ uthere/src/settings.js
@@ -1,7 +1,7 @@
 import {createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "8e18069b8cd34fc699421190d903f30b";
-const token = "007eJxTYNh6rq+1MdesOmlefdjyAO7PvtrXAv8kPf2///cuR/EOSy0FBotUQwsDM8ski+QUY5O0ZDNLSxMjQ0NLgxRLA+M0Y4OktZ/dUxoCGRnWL5RkZGSAQBCfjaG0JCO1KJWBAQDWEyEC";
+const token = "007eJxTYMhu7QhYNbFy2dYp4gY2jptXtB/zUJh9k8/qg2rVuj3su8MUGCxSDS0MzCyTLJJTjE3Sks0sLU2MDA0tDVIsDYzTjA2SUrb7pjQEMjJkZ39lYmSAQBCfjaG0JCO1KJWBAQBJ8R9b";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token, certificate:"e774cf41127045cc8332e0943611941e" };
 
