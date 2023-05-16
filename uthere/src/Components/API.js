e1e82501bb98a71a9acc2c93c733b291e6f937fe
Kimya Ghasemlou <kimya.ghasemloo@gmail.com>
2023-05-16 03:41:56 +0300

    Update API.js

--- uthere/src/Components/API.js
+++ uthere/src/Components/API.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 const token = localStorage.getItem('token');
-import { getCSRFToken } from './utils';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-const csrfToken = getCSRFToken();
 export default axios.create({
     // CHANGE THE BASE URL TO TEST IN LOCAL ENVIRONMENT
     // BaseURL: "http://127.0.0.1:8000/api/"
@@ -13,7 +11,5 @@ export default axios.create({
         'Accept':'application/json',
         Authorization: `Token ${token}`,
         'Content-Type':'application/json',
-        'X-CSRFToken': csrfToken,
-
     }
 });
