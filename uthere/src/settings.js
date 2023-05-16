273390fd330b003f88b7a3e8ef54b00427430439
Kimya Ghasemlou <kimya.ghasemloo@gmail.com>
2023-05-16 04:40:00 +0300

    Update settings.js

--- uthere/src/settings.js
+++ uthere/src/settings.js
@@ -1,6 +1,6 @@
 import {createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
-const appId = "c3154f9b5b964dc8a4392eaa5f14e808\n";
+const appId = "c3154f9b5b964dc8a4392eaa5f14e808";
 const token = "007eJxTYPBr/ZeTx9PA5/c4/0Wp7rZLv+18rLJblbIkGGY2/7x7a4UCQ7KxoalJmmWSaZKlmUlKskWiibGlUWpiommaoUmqhYFF2LWklIZARob9bPrMjAwQCOKzMZSWZKQWpTIwAACD3x/s";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token, certificate:"93df7041203e44a2b1e60bf80dd4c8fa" };
