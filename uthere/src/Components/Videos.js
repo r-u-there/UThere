7cd9ce1863cfdaf65aeafe415e107b09ecfd5aea
melike1818 <mlkdemirci18@gmail.com>
2023-05-21 18:57:42 +0300

    Update Videos.js

--- uthere/src/Components/Videos.js
+++ uthere/src/Components/Videos.js
@@ -144,6 +144,7 @@ function Videos(props) {
 						const { uid, participantName, isScreenShare, video } = userData;
 						if (video) {
 							console.log(isScreenShare);
+							console.log(participantName);
 							if(!isScreenShare){
 								console.log(1 +" "+ uid)
 								return (
