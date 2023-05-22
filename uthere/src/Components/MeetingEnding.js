62daf2b6c8eb12d518fbf90d444635b26781c0a0
İlke Kaş <ilkeekas@gmail.com>
2023-05-22 05:15:33 +0300

    de
    de

--- uthere/src/Components/MeetingEnding.js
+++ uthere/src/Components/MeetingEnding.js
@@ -23,8 +23,9 @@ function MeetingEnding() {
 
     setTimeout(() => {
       setLoading(true);
+      window.location.reload();
     }, 6000); // Replace with your actual data fetching logic
-    window.location.reload();
+  
   }, []);
 
   function giveShareAccess(id, agora_id) {
