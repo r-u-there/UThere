759380b5cd3ec3b4796396d290873dbc50165f0a
Kimya Ghasemlou <kimya.ghasemloo@gmail.com>
2023-05-31 06:31:48 +0300

    new frontend domain

--- uthere-backend/UthereBackend/UthereBackend/settings.py
+++ uthere-backend/UthereBackend/UthereBackend/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://uthere-git-27-adding-ml-server-to-cloud-uthere.vercel.app"
+    "https://u-there-seven.vercel.app/"
 ]
 
 CORS_ALLOW_METHODS = [
